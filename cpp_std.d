fc6f50375ea32119f3661a1cd30bb28603e54f04
Guillaume Chatelet <chatelet.guillaume@gmail.com>
2015-01-29 22:44:36 +0100

    First version, fails to link

---
+++ cpp_std.d
@@ -0,0 +1,40 @@
+ 
+pragma(lib, "/lib/libstdc++.a"); // get all symbols with "nm -Cgnu -g -A /lib/libstdc++.a"
+
+extern (C++, __gnu_cxx) {
+
+  struct new_allocator(T) {
+    alias size_type = size_t;
+    void deallocator(T*, size_type);
+  }
+
+}
+
+extern(C++, std) {
+
+  struct allocator(T) {
+    alias size_type = size_t;
+  }
+
+  struct char_traits(CharT) {
+  }
+  
+  class basic_string(T, TRAITS = char_traits!T, ALLOC = allocator!T) {
+    alias size_type = size_t;
+    enum size_t npos = size_t.max;
+    
+    this();
+    this(const(T*) _, const ref allocator!T _);
+    
+    private void[8] _; // should probably match sizeof(std::string) to pad the object correctly.
+  }
+  
+  alias basic_string!char string;
+  alias basic_string!wchar wstring;
+
+}
+
+void main() {
+  auto s = new std.string();
+  // undefined reference to std::basic_string<char, std::char_traits<char>, std::allocator<char> >::__ctor()
+}
